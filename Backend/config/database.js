6000d2cbea6f34d01a72b81567278d5e81a83e74
gaurav-opensource <gauravopensource95@gmail.com>
2026-01-23 13:25:22 +0530

    improve MongoDB connection logging

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
 
+
 // connect mongodb database
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`MongoDB successfully connected : ${connection.connection.host}`);
+    // eslint-disable-next-line no-console
+    console.log(`Mongo DB Successfully Connected : ${connection.connection.host}`);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
     process.exit(1);
   }
 };
+
 
 module.exports = connectDB;
