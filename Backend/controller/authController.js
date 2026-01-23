4962bb84708d2c11be451ecd94a51b85814eb1a3
gaurav-opensource <gauravopensource95@gmail.com>
2026-01-23 13:52:36 +0530

    enhance authentication validation logic

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../model/user.model');
 const { HrSignupSchema } = require("../config/schema");
-const { studentRegisterSchema,loginSchema } = require("../config/schema");
+const { studentRegisterSchema, loginSchema } = require("../config/schema");
 
 
 // Login for both Student and HR
@@ -33,15 +33,10 @@ exports.login = async (req, res) => {
     );
 
     res.status(200).json({
+      message: "Login successful",
       token,
-      role: user.role,  
-      user: {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        location: user.location,
-        phone: user.phone
-      }
+      role: user.role,
+      userId: user._id,
     });
   } catch (error) {
     return res.status(500).json({ message: error.message });
@@ -49,23 +44,24 @@ exports.login = async (req, res) => {
 };
 
 
-// Register HR
+// Register HR 
 exports.registerHR = async (req, res) => {
+  // Validate request body
   const { error } = HrSignupSchema.validate(req.body);
-  console.log("Validation Error:", error);
+
   if (error) {
     return res.status(400).json({ message: error.details[0].message });
   }
 
   try {
     const { name, email, password, contact,companyName, position } = req.body;
-
-
     const existingHR = await User.findOne({ email });
+
+    // Check if HR with the same email already exists
     if (existingHR) {
       return res.status(400).json({ message: "Email already exists" });
     }
-
+    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const hr = new User({
@@ -89,7 +85,10 @@ exports.registerHR = async (req, res) => {
 
 // Register Student
 exports.register = async (req, res) => {
+  // Validate request body
   const { error } = studentRegisterSchema.validate(req.body);
+
+  // If validation fails, return error
   if (error) {
     return res.status(400).json({ message: error.details[0].message });
   }
@@ -115,10 +114,12 @@ exports.register = async (req, res) => {
     } = req.body;
 
     const existingStudent = await User.findOne({ email });
+
+    // Check if student with the same email already exists
     if (existingStudent) {
       return res.status(400).json({ message: "Email already exists!" });
     }
-
+    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newStudent = new User({
@@ -141,10 +142,10 @@ exports.register = async (req, res) => {
       resume,
       location
     });
-
+    // Save the new student to the database
     await newStudent.save();
 
-    res.status(201).json({ message: "Student registered successfully." });
+    res.status(201).json({ message: " Student registered successfully" });
   } catch (error) {
     console.error("Register Error:", error.message);
     res.status(500).json({ message: "Server error" });
